adec20a068b53e6f3e04c96518b6f8d4ef033940
Unknown <belcegorx@gmail.com>
2018-05-11 11:11:01 +0200

    fh

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Noticias-final
+noticias
---
+++ js/news.js
@@ -0,0 +1,19 @@
+
+$.getJSON("https://rawgit.com/urbinapro/news/master/data/names.json", function (jsonObject) {
+    //en aquest punt l'objecte jsonObject correspon al fitxer
+    pintar(jsonObject);
+});
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+            alert("near bottom!");
+        }
+    })
+});
+
+function pintar(json) {
+    $.each(json, function (i, empleado) {
+        $("#out").append("<li id='" + i + "'>" + empleado.firstName + " " + empleado.lastName + "</li>");
+    });
+}
