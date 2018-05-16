7e92e61fc81b1620f2923c89ae97556ed8397bf1
Unknown <belcegorx@gmail.com>
2018-05-16 20:26:13 +0200

    Auto stash before merge of "master" and "origin/master"

--- js/news.js
+++ js/news.js
@@ -1,24 +1,14 @@
 
-$.getJSON("https://rawgit.com/urbinapro/news/master/data/names.json", function (jsonObject) {
-    //en aquest punt l'objecte jsonObject correspon al fitxer
-    pintar(jsonObject);
-});
 
 $(document).ready(function () {
     $(window).scroll(function () {
-        if ($(window).scrollTop() + $(window).height() <= $(document).height() + 10) {  /*miar bien como va lo del espacio*/
-            $(".noticias").append(
-                '<div class="col-sm-6 noticia">'
-                + '<div class="jumbotron">'
-                + '<a href="news3.html" id="news3">'
-                + '<h2>' + + '</h2>'
-                + '<span>' + '<img>' + '</span>' //imagen
-                + '<br>'
-                + '<span>' + + '<span>' //fecha
-                + '<p>' + + '</p>' //descripción
-                + '</div>'
-                + '</div>'
-            );
+        if ($(window).scrollTop() + $(window).height() > $(document).height() - 10) {  /*miar bien como va lo del espacio*/
+
+            $.getJSON("https://cdn.rawgit.com/Pauuu/Noticias-final/2cd0807f/data/data1.json", function (jsonObject) {
+                //en aquest punt l'objecte jsonObject correspon al fitxer
+                pintarNoticias(jsonObject);
+            });
+            
         }
     })
 });
@@ -27,13 +17,11 @@ function pintar(json) {
     $.each(json, function (i, empleado) {
         $("#out").append("<li id='" + i + "'>" + empleado.firstName + " " + empleado.lastName + "</li>");
     });
-
-
 }
 
 function pintarNoticias(json) {
     $.each(json, function (i, noticia) {
-        $("#out").append(
+        $(".noticias").append(
             '<div class="col-sm-6 noticia">'
             + '<div class="jumbotron">'
             + '<a href="news3.html" id="news3">'
