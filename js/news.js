973b0a9862c17a7cf571b03806b9b927ac1b1849
Unknown <belcegorx@gmail.com>
2018-05-16 14:20:41 +0200

    Añadidos archivos json vacios

--- js/news.js
+++ js/news.js
@@ -6,7 +6,7 @@ $.getJSON("https://rawgit.com/urbinapro/news/master/data/names.json", function (
 
 $(document).ready(function () {
     $(window).scroll(function () {
-        if ($(window).scrollTop() + $(window).height() <= $(document).height() + 10) {  /*miar bien como va lo del espacio*/ 
+        if ($(window).scrollTop() + $(window).height() <= $(document).height() + 10) {  /*miar bien como va lo del espacio*/
             $(".noticias").append(
                 '<div class="col-sm-6 noticia">'
                 + '<div class="jumbotron">'
@@ -26,5 +26,24 @@ $(document).ready(function () {
 function pintar(json) {
     $.each(json, function (i, empleado) {
         $("#out").append("<li id='" + i + "'>" + empleado.firstName + " " + empleado.lastName + "</li>");
+    });
+
+
+}
+
+function pintarNoticias(json) {
+    $.each(json, function (i, noticia) {
+        $("#out").append(
+            '<div class="col-sm-6 noticia">'
+            + '<div class="jumbotron">'
+            + '<a href="news3.html" id="news3">'
+            + '<h2>' + + '</h2>'
+            + '<span>' + '<img>' + '</span>' //imagen
+            + '<br>'
+            + '<span>' + + '<span>' //fecha
+            + '<p>' + + '</p>' //descripción
+            + '</div>'
+            + '</div>'
+        );
     });
 }
