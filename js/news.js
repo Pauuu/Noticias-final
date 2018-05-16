314adff9aa68122928c05f694ae53621bfaa3641
Unknown <belcegorx@gmail.com>
2018-05-16 20:49:43 +0200

    Mirar porque no carga el json

--- js/news.js
+++ js/news.js
@@ -4,28 +4,21 @@ $(document).ready(function () {
     $(window).scroll(function () {
         if ($(window).scrollTop() + $(window).height() > $(document).height() - 10) {  /*miar bien como va lo del espacio*/
 
-            $.getJSON("https://cdn.rawgit.com/Pauuu/Noticias-final/2cd0807f/data/data1.json", function (jsonObject) {
+            $.getJSON("https://rawgit.com/urbinapro/news/master/data/names.json", function (jsonObject) {
                 //en aquest punt l'objecte jsonObject correspon al fitxer
-                pintarNoticias(jsonObject);
+                mostrarNoticia(jsonObject);
             });
             
         }
     })
 });
 
-function pintar(json) {
-    $.each(json, function (i, empleado) {
-        $("#out").append("<li id='" + i + "'>" + empleado.firstName + " " + empleado.lastName + "</li>");
-    });
-}
-
-function pintarNoticias(json) {
-    $.each(json, function (i, noticia) {
+function mostrarNoticia(json) {
+    $.each(json, function (i, item) {
         $(".noticias").append(
             '<div class="col-sm-6 noticia">'
             + '<div class="jumbotron">'
-            + '<a href="news3.html" id="news3">'
-            + '<h2>' + + '</h2>'
+            + '<h2>' + item.empleado + '</h2>'
             + '<span>' + '<img>' + '</span>' //imagen
             + '<br>'
             + '<span>' + + '<span>' //fecha
@@ -33,5 +26,5 @@ function pintarNoticias(json) {
             + '</div>'
             + '</div>'
         );
-    });
+    })
 }
