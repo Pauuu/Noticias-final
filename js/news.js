343a9ce93236fd8bb99a1bc9bc5f51254a6e4124
Unknown <belcegorx@gmail.com>
2018-05-17 20:06:17 +0200

    Añadido rss

--- js/news.js
+++ js/news.js
@@ -1,24 +1,38 @@
 
+var automatico = false; //variable booleana; ('1')true si carga automática, ('0')false si con botón
+
 
 $(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(window).scrollTop() + $(window).height() > $(document).height() - 10) {  /*miar bien como va lo del espacio*/
+    $("#botonCargaAutomatica").click(function () {
+        automatico = true;
+        alert(automatico);
+    });
 
-            $.getJSON("https://rawgit.com/urbinapro/news/master/data/names.json", function (jsonObject) {
-                //en aquest punt l'objecte jsonObject correspon al fitxer
-                mostrarNoticia(jsonObject);
-            });
-            
-        }
-    })
+    if (automatico == true) {
+        mostrarNoticiaOnScroll();
+    }
 });
 
+function cargarNoticias() {
+
+
+    $("#botonCargaAutomatica").click(function () {
+        automatico = (automatico + 1) % 2;
+        alert(automatico);
+    });
+
+    if (automatico == 1) {
+        mostrarNoticiaOnScroll();
+    }
+}
+
+//imprime por pantalla la la noticia junto con las etiquetas html correspondientes
 function mostrarNoticia(json) {
     $.each(json, function (i, item) {
         $(".noticias").append(
             '<div class="col-sm-6 noticia">'
             + '<div class="jumbotron">'
-            + '<h2>' + item.empleado + '</h2>'
+            + '<h2 class="titular">' + item.empleado + '</h2>'
             + '<span>' + '<img>' + '</span>' //imagen
             + '<br>'
             + '<span>' + + '<span>' //fecha
@@ -26,5 +40,18 @@ function mostrarNoticia(json) {
             + '</div>'
             + '</div>'
         );
+    })
+}
+
+//carga noticias automáticamente cuando el usuario esté llegando al final de la página 
+function mostrarNoticiaOnScroll() {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() + $(window).height() > $(document).height() - 20) {
+
+            $.getJSON("https://rawgit.com/urbinapro/news/master/data/names.json", function (jsonObject) {
+                //en aquest punt l'objecte jsonObject correspon al fitxer
+                mostrarNoticia(jsonObject);
+            });
+        }
     })
 }
