3c3475be30e13c101504c5979b6920721a6a991f
Pau <belcegorx@gmail.com>
2018-05-21 00:08:59 +0200

    Boton de carga automatica creado y funcional

--- js/news.js
+++ js/news.js
@@ -1,30 +1,28 @@
 
 var automatico = false; //variable booleana; ('1')true si carga automática, ('0')false si con botón
 
-
 $(document).ready(function () {
+
     $("#botonCargaAutomatica").click(function () {
-        automatico = true;
-        alert(automatico);
+        if (automatico == true) {
+
+            automatico = false;
+            alert("carga automatica desactivada");
+
+        } else {
+            automatico = true;
+            alert("carga automatica activada");
+        }
+
     });
 
-    if (automatico == true) {
-        mostrarNoticiaOnScroll();
-    }
+    $(window).scroll(function () {
+        if (automatico) {
+            mostrarNoticiaOnScroll();
+        }
+    })
+
 });
-
-function cargarNoticias() {
-
-
-    $("#botonCargaAutomatica").click(function () {
-        automatico = (automatico + 1) % 2;
-        alert(automatico);
-    });
-
-    if (automatico == 1) {
-        mostrarNoticiaOnScroll();
-    }
-}
 
 //imprime por pantalla la la noticia junto con las etiquetas html correspondientes
 function mostrarNoticia(json) {
@@ -32,7 +30,7 @@ function mostrarNoticia(json) {
         $(".noticias").append(
             '<div class="col-sm-6 noticia">'
             + '<div class="jumbotron">'
-            + '<h2 class="titular">' + item.empleado + '</h2>'
+            + '<h2 class="titular">' + item + '</h2>'
             + '<span>' + '<img>' + '</span>' //imagen
             + '<br>'
             + '<span>' + + '<span>' //fecha
@@ -45,13 +43,13 @@ function mostrarNoticia(json) {
 
 //carga noticias automáticamente cuando el usuario esté llegando al final de la página 
 function mostrarNoticiaOnScroll() {
-    $(window).scroll(function () {
-        if ($(window).scrollTop() + $(window).height() > $(document).height() - 20) {
 
-            $.getJSON("https://rawgit.com/urbinapro/news/master/data/names.json", function (jsonObject) {
-                //en aquest punt l'objecte jsonObject correspon al fitxer
-                mostrarNoticia(jsonObject);
-            });
-        }
-    })
+    if ($(window).scrollTop() + $(window).height() > $(document).height() - 20) {
+
+        $.getJSON("https://rawgit.com/Pauuu/Noticias-final/master/data/data" + 1 + ".json", function (jsonObject) {
+            //en aquest punt l'objecte jsonObject correspon al fitxer
+            mostrarNoticia(jsonObject);
+        });
+
+    }
 }
